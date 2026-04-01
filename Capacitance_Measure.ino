5a9c0c1ff955710303b48618b89ab5e7a92f5a1a
Prajeet Kumar A <kakarotff902@gmail.com>
2026-04-01 23:58:08 +0530

    Update Capacitance_Measure.ino

--- Capacitance_Measure.ino
+++ Capacitance_Measure.ino
@@ -1,5 +1,3 @@
-//this is a prototype code, complete code will be updated
-
 int D8 = 8;
 int D9 = 9;
 int D10 = 10;
@@ -16,58 +14,84 @@ double R3 = 680+24.8;
 double R4 = 470000+5.46;
 
 double Cx = 0;
-float t = 0;
+double t = 0;
 double st = 0;
 float Vx = 0;
+
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(TP1, INPUT);
+  pinMode(TP2, INPUT);
 }
 
-void discharge(){
+
+void loop() {
+  discharge();
+  delay(500); 
+  charge();
+
+  // FIX: Added parentheses around (R1 + R3)
+  // Without parentheses, it divides t by R1 then adds R3 to the result.
+  Cx = ((double)t / 1000.0) / (R2 + R3); 
+  
+  Serial.print("Capacitance : ");
+  Serial.println(Cx * 1000000, 12);
+  Serial.print("Time (ms): "); // Note: millis() gives ms, not us
+  Serial.println(t, 3);
+  Serial.println("--------------------------------------");
+  
+  delay(3000);
+}
+
+void discharge() {
   Serial.println("Discharging");
-  pinMode(D9, OUTPUT);          //470K
-  pinMode(D10, OUTPUT);         //680
-  pinMode(D8, OUTPUT);            //680
-  pinMode(D11, OUTPUT);           //470K
-
-  digitalWrite(D9, LOW);
-  digitalWrite(D10, LOW);
-  digitalWrite(D8, LOW);
-  digitalWrite(D11, LOW);
-  while(analogRead(TP1)>0);
-}
-
-void charge(){
-  Serial.println("charging");
-  //pinMode(D9, OUTPUT);
-  //pinMode(D10, OUTPUT);
   pinMode(D8, OUTPUT);
+  pinMode(D9, OUTPUT);
+  pinMode(D10, OUTPUT);
   pinMode(D11, OUTPUT);
 
-  //digitalWrite(D9, HIGH);
-  //digitalWrite(D10, LOW);
-  digitalWrite(D8, HIGH);
+  digitalWrite(D8, LOW);
+  digitalWrite(D9, LOW);
+  digitalWrite(D10, LOW);
   digitalWrite(D11, LOW);
+
+  // FIX: Using > 2 instead of > 0 to prevent code from hanging on ADC noise
+  while (analogRead(TP1) > 2); 
+
+  Serial.println("Discharging");
+
+  pinMode(D8, INPUT);
+  pinMode(D9, INPUT);
+  pinMode(D10, INPUT);
+  pinMode(D11, INPUT);
+  delay(100);
+}
+
+void charge() {
+  Serial.println("charging");
   
+  pinMode(D9, OUTPUT);
+  pinMode(D10, OUTPUT);
+  //pinMode(D8, OUTPUT);
+  //pinMode(D11, OUTPUT);
+
+  digitalWrite(D9, HIGH);
+  digitalWrite(D10, LOW);
+  //digitalWrite(D8, HIGH);
+  //digitalWrite(D11, LOW);
+
   st = millis();
-  Serial.println(analogRead(TP1));
-  while(analogRead(TP1) <= 646){
+  
+  //Serial.println(analogRead(TP1));
+  while (analogRead(TP1) < 648) { 
     ADC_1 = analogRead(TP1);
     Vx = ADC_1 * 0.004882;
     Serial.println(Vx);
-
   }
+  
   t = millis() - st;
+  
+  delay(100);
+  //Serial.println((float)t);
   //Vx = ADC_1 * 0.004882;
-
-}
-
-void loop(){
-  discharge();
-  charge();
-  Cx = (t/1000)/(R1);
-  Serial.println("Capacitance: "+ String(Cx, 12));
-  Serial.println("Time: "+String(t));
-  Serial.println("-------------------------------------------------");
-
 }
